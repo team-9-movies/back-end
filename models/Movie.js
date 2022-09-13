8d503dfe71b581332b6fd6e22fe502979a9c975f
alice <aliceahuh213@gmail.com>
2022-09-13 15:06:56 -0400

    add movie schema

--- models/Movie.js
+++ models/Movie.js
@@ -1,1 +1,40 @@
-//movie schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const MovieSchema = new Schema(
+{
+    title:
+    {
+        type:String,
+        required: true
+    },
+    imgUrl:
+    {
+        type: String,
+    },
+    trailerUrl:
+    {
+        type: String,
+    },
+    director:
+    {
+        type: String,
+    },
+    year:
+    {
+        type: String,
+    },
+    review:
+    {
+        type: String,
+    },
+    user:
+    {
+        type:Schema.ObjectId,
+        ref: "User"
+    },
+
+});
+
+module.exports = Movie = mongoose.model("Movie", MovieSchema);
